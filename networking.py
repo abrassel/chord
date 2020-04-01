063b5059b84c2c2e8176505a06abfd0a7827b90a
abrassel <abrassel@umd.edu>
2020-04-01 00:21:12 -0400

    set up networking framework.

--- networking.py
+++ networking.py
@@ -4,16 +4,16 @@ from enum import Enum, unique
 
 @unique
 class OpCode(Enum):
-    SET = 0
-    GET = 1
-    FIND_SUCCESSOR = 2
-    FIND_PREDECESSOR = 3
-    PREDECESSOR = 4
-    CHECK = 5
-    NOTIFY_OF_LEAVE = 6
-    NOTIFY = 7
-    GET_FINGER_TABLE = 8
-    GIVE_FINGER = 9
+    SET = "0"
+    GET = "1"
+    FIND_SUCCESSOR = "2"
+    FIND_PREDECESSOR = "3"
+    PREDECESSOR = "4"
+    CHECK = "5"
+    NOTIFY_OF_LEAVE = "6"
+    NOTIFY = "7"
+    GET_FINGER_TABLE = "8"
+    GIVE_FINGER = "9"
 
 response_required = set(
     OpCode.GET,
@@ -23,12 +23,44 @@ response_required = set(
     OpCode.CHECK,
     OpCode.GET_FINGER_TABLE
 )
-    
+
+
+# TODO: multi-thread acceptance and sending.
 class Network:
 
-    def __init__(self):
+    def __init__(self, ip, host, owner_ip = None, owner_host = None):
+        self.ip = ip
+        self.host = host
+
+
+        if not owner_ip or not owner_host:
+            self.__setup_networking()
+        
+        
+        if dest_ip and dest_host:
+            self.__create_connection()
+            
+        
+    def tcp_send(self, message):
+        """
+        Send the message to the node.  Guarantee arrival.
+        """
         pass
 
+
+    def udp_send(self, message):
+        """
+        Send the message to the node.  No guarantees about arrival.
+        """
+        pass
+
+
+    def await_response(self):
+        """
+        Get a response from  the node on the far side.
+        """
+        pass
+        
 
     @staticmethod
     def remote(func):
@@ -46,13 +78,59 @@ class Network:
 
     @staticmethod
     def sendme(func):
+        """
+        Sends the parameters given to the remote node
+        """
+        
         def wrapper(peer, args):
-            opcode = func(peer, args)
+            result = func(peer, args)
+            opcode = result[0]
+            message = serialize(result)
+            peer.network.tcp_send(message)
             if opcode in response_required:
-                peer.network.tcp_send(args)
-                return peer.network.wait_response()
+                return deserialize(opcode, peer.network.wait_response())
             else:
-                peer.network.udp_send(args)
+                peer.network.udp_send(opcode, message)
 
         return wrapper
         
+
+    @staticmethod
+    def serialize(args):
+        result = ""
+        for arg in args:
+            if isinstance(arg, str):
+                result += arg
+            elif isinstance(arg, Node):
+                result += (
+                    arg.networking.ip
+                    + "|" + arg.networking.host
+                    + "|" + arg.key
+                )
+            else:
+                result += arg.serialize()
+
+            result += "|"
+
+        return result[:-1]  # truncate last separator
+
+
+    @staticmethod
+    def unserialize(opcode, response):
+        pass
+        
+
+    def __create_connection(self):
+        """
+        Used when this is a peer
+        and want to abstract connection to it.
+        """
+        pass
+
+
+    def __setup_networking(self):
+        """
+        Used when you own the host, port.
+        Bind to it so that others can connect to you.
+        """
+        pass
--- peer_node.py
+++ peer_node.py
@@ -13,12 +13,12 @@ class PeerNode(Node):
 
     @sendme
     def __setitem__(self, key, value):
-        return OpCode.SET
+        return (OpCode.SET, key, value)
 
 
     @sendme
     def __getitem__(self, key):
-        return OpCode.GET
+        return (OpCode.GET, key)
 
 
     @sendme
@@ -26,7 +26,7 @@ class PeerNode(Node):
         """
         Obtain the successor node for the given key
         """
-        return OpCode.FIND_SUCCESSOR
+        return (OpCode.FIND_SUCCESSOR, key)
 
 
     @sendme
@@ -34,7 +34,7 @@ class PeerNode(Node):
         """
         Obtain the predecessor node for the given key
         """
-        return Opcode.FIND_PREDECESSOR
+        return (Opcode.FIND_PREDECESSOR, key)
 
 
     @sendme
@@ -42,7 +42,7 @@ class PeerNode(Node):
         """
         Return own predecessor node
         """
-        return OpCode.PREDECESSOR
+        return (OpCode.PREDECESSOR,)
 
 
     @sendme
@@ -50,7 +50,7 @@ class PeerNode(Node):
         """
         Determine if this node is alive or not
         """
-        return OpCode.CHECK
+        return (OpCode.CHECK,)
 
 
     @sendme
@@ -60,7 +60,7 @@ class PeerNode(Node):
         Gain a new successor and tell them that you
         are now the predecessor.
         """
-        return OpCode.NOTIFY_OF_LEAVE
+        return (OpCode.NOTIFY_OF_LEAVE, self.owner, new_successor)
 
 
     @sendme
@@ -69,7 +69,7 @@ class PeerNode(Node):
         Be notified by your owner that you
         may have a new predecessor.
         """
-        return OpCode.NOTIFY
+        return (OpCode.NOTIFY, self.owner)
 
 
     @sendme
@@ -78,7 +78,7 @@ class PeerNode(Node):
         Give the finger table to the caller
         so that they can set up their own.
         """
-        return OpCode.GET_FINGER_TABLE
+        return (OpCode.GET_FINGER_TABLE,)
 
 
     @sendme
@@ -87,7 +87,7 @@ class PeerNode(Node):
         Tell this node that their finger
         should be the owner.
         """
-        return GIVE_FINGER
+        return (GIVE_FINGER, self.owner)
 
 
     
